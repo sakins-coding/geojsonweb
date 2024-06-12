2cad8a68e7aa9dfaf11e44401378067201b625e5
Scott Akins <sakins@nmsu.edu>
2024-06-12 12:12:44 -0600

    Initial check in of current progress

---
+++ js/geojsonmap.js
@@ -0,0 +1,45 @@
+let geodata;
+
+async function getGeoJSON (url) {
+  $.ajax({
+    url: url,
+    async: false,
+    dataType: 'json',
+    success: function (response) {
+      geodata = response;
+    }
+  });
+}
+
+function atlas (mapdiv="map", id_field="id", detailurl="detail.html") {
+  const map = L.map(mapdiv).setView([35, -109], 7);
+
+	// const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	// 	maxZoom: 19,
+	// 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	// }).addTo(map);
+  const tiles = L.tileLayer('https://a.tile.opentopomap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; Open Topo Map'
+	}).addTo(map);
+
+	function onEachFeature(feature, layer) {
+		let popupContent = `<p>This artwork is titled <br/><a href="${detailurl}?id=${feature.properties.id}">${feature.properties.name}</p><img src="${feature.properties.image}" width=300px></a>`;
+
+		if (feature.properties && feature.properties.popupContent) {
+			popupContent += feature.properties.popupContent;
+		}
+
+		layer.bindPopup(popupContent);
+	}
+
+  const pleinWhereLayer = L.geoJSON(geodata, {
+
+		pointToLayer(feature, latlng) {
+			return L.marker(latlng);
+		},
+
+		onEachFeature
+	}).addTo(map);
+
+}
